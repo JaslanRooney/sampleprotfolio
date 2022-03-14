a9808721e8b65a7a8a4d4fdacf589e263b000f93
Jaslan Rooney <jaslan.rune.dev@gmail.com>
2022-03-14 12:29:57 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// const path = require('path')
+// const express = require('express');
+// const app = express();
+// const ejs = require('ejs')
+
+
+
+
+// const viewsPath = path.join(__dirname, '../views');
+// app.set('views', viewsPath)
+// app.set('view engine', 'ejs');
+
+// app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+
+// app.get('/', function (req, res) {
+//     res.render('index')
+// })
+
+// app.get('/about', function (req, res) {
+//     res.send("Hello about page")
+// })
+
+
+
+// app.listen(3000, () => {
+//     console.log("server is running port 3000")
+// })
